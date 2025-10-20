5b2d4157668552d94b6ec4c2b2513fa45f96d47a
raptaro <136423974+raptaro@users.noreply.github.com>
2025-10-20 21:48:07 +0800

    fix routing

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,33 +1,18 @@
-"""
-ASGI config for backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
-# your_project/asgi.py
-
-# backend/asgi.py
-
 import os
 import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-import queueing.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 django.setup()
 
+# Import your websocket routing after django.setup()
+import queueing.routing
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            queueing.routing.websocket_urlpatterns
-        )
+        URLRouter(queueing.routing.websocket_urlpatterns)
     ),
 })
-
-
