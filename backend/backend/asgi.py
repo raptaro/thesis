089d3201a61a03b63a951881dc17f0e0bfeadfae
raptaro <136423974+raptaro@users.noreply.github.com>
2025-10-20 23:38:21 +0800

    fix

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -7,7 +7,6 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 django.setup()
 
-# Import your websocket routing after django.setup()
 import queueing.routing
 
 application = ProtocolTypeRouter({
--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,4 @@
-]
+
 set -e
 
 echo "Waiting for database..."
